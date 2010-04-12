634d7e9276ed1b7486436af4251c7159853455aa
mmbt <mmbt@cafd5db9-86ca-4bab-b214-9173f0b6ba26>
2010-04-12 00:07:43 +0000

    will way
    git-svn-id: http://m2.daffodil.uk.com/svn/ffs@11 cafd5db9-86ca-4bab-b214-9173f0b6ba26

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Serving localhost:8080 "
 ./gae/dev_appserver.py ./freeflightsim.appspot.com
 
 
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "Updating -  freeflightsim.appspot.com"
 ./gae/appcfg.py  update ./freeflightsim.appspot.com
 
